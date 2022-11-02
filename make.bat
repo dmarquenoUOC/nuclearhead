97dad858574c0ffbe54b124893e695f241f1933e
dmarquenoUOC <116842281+dmarquenoUOC@users.noreply.github.com>
2022-11-02 22:54:31 +0100

    cambios de compilación

--- make.bat
+++ make.bat
@@ -1,3 +1,11 @@
+REM delete previous files
+DEL rom/*.gb
+REM graphics
+lcc -Wf-bo1 -c dev/data/tiles/bkg_tileset.c -o bkg_tileset.o
+lcc -Wf-bo1 -c dev/data/tiles/gui_tileset.c -o gui_tileset.o
+REM maps
+lcc -Wf-bo2 -c dev/data/maps/bkg_map.c -o bkg_map.o
+lcc -Wf-bo2 -c dev/data/maps/gui_map.c -o gui_map.o
 REM logic
 lcc -c dev/main.c -o main.o
 REM build
