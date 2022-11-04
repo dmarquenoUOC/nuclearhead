270a26a971475f7dbce6075033ae25dd562a21b3
dmarquenoUOC <116842281+dmarquenoUOC@users.noreply.github.com>
2022-11-04 20:16:57 +0100

    Se descarta el uso de la capa Window

--- dev/main.c
+++ dev/main.c
@@ -5,10 +5,10 @@
 
 // Tiles
 #include "data/tiles/bkg_tileset.h"
-#include "data/tiles/gui_tileset.h"
+//#include "data/tiles/gui_tileset.h"
 // Maps
 #include "data/maps/bkg_map.h"
-#include "data/maps/gui_map.h"
+//#include "data/maps/gui_map.h"
 
 //Variables
 UBYTE bkg_x;
@@ -44,7 +44,7 @@ void init() {
     DISPLAY_OFF;
     init_game_var();
     init_game_bkg();
-    init_game_gui();
+    //init_game_gui();
     DISPLAY_ON;
     enable_interrupts();
 
@@ -56,19 +56,21 @@ void init_game_var() {
 }
 
 void init_game_bkg() {
-    SWITCH_ROM_MBC1(bkg_tilesetBank);
-    set_bkg_data(0, 16, bkg_tileset); //Load 16 Tiles in BackGround VRAM
-    SWITCH_ROM_MBC1(bkg_mapBank);
-    set_bkg_tiles(0, 0, 32, 32, bkg_map);
+    SWITCH_ROM_MBC1(bkg_tilesetBank);                               //Select Bank Memory
+    set_bkg_data(0, 16, bkg_tileset);                               //Load 16 Tiles in VRAM
+    SWITCH_ROM_MBC1(bkg_mapBank);                                   //Select Banck Memory
+    set_bkg_tiles(0, 0, bkg_mapWidth, bkg_mapHeight, bkg_map);      //Set tiles into Background Layer
     move_bkg(bkg_x, bkg_y);
     SHOW_BKG;
 }
 
+/*
 void init_game_gui() {
-    SWITCH_ROM_MBC1(gui_tilesetBank);
-    set_win_data(128, 16, gui_tileset); //Load 16 Tiles in Window VRAM
-    SWITCH_ROM_MBC1(gui_mapBank);
-    set_win_tiles(0, 0, gui_mapWidth, gui_mapHeight, gui_map);
+    SWITCH_ROM_MBC1(gui_tilesetBank); //Select Bank Memory
+    set_win_data(128, 16, gui_tileset); //Load 16 Tiles in VRAM
+    SWITCH_ROM_MBC1(gui_mapBank); //Select Banck Memory
+    set_win_tiles(0, 0, gui_mapWidth, gui_mapHeight, gui_map); //Set tiles into Window Layer
     move_win(7, 128);
     SHOW_WIN;
 }
+*/
--- make.bat
+++ make.bat
@@ -1,13 +1,11 @@
 REM graphics
 lcc -Wf-bo1 -c dev/data/tiles/bkg_tileset.c -o bkg_tileset.o
-lcc -Wf-bo1 -c dev/data/tiles/gui_tileset.c -o gui_tileset.o
 REM maps
 lcc -Wf-bo2 -c dev/data/maps/bkg_map.c -o bkg_map.o
-lcc -Wf-bo2 -c dev/data/maps/gui_map.c -o gui_map.o
 REM logic
 lcc -c dev/main.c -o main.o
 REM build
-lcc -Wl-yt1 -Wl-yo4 -Wl-ya0 -o rom/NuclearHead.gb main.o bkg_tileset.o gui_tileset.o bkg_map.o gui_map.o
+lcc -Wl-yt1 -Wl-yo4 -Wl-ya0 -o rom/NuclearHead.gb main.o bkg_tileset.o bkg_map.o
 REM clean
 del *.o *.lst
 pause
