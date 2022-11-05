9bdc45926129791f054ea11bc23217c8ed3f869a
dmarquenoUOC <116842281+dmarquenoUOC@users.noreply.github.com>
2022-11-05 02:40:39 +0100

    Añadir PG

--- dev/main.c
+++ dev/main.c
@@ -5,21 +5,37 @@
 
 // Tiles
 #include "data/tiles/bkg_tileset.h"
+#include "data/tiles/pg_idle_tileset.h"
 //#include "data/tiles/gui_tileset.h"
 // Maps
 #include "data/maps/bkg_map.h"
 //#include "data/maps/gui_map.h"
 
+// rutinas de manejo de metasprites (sprite formado por varios sprites)
+#include "utils/char_utils.h"
+
 //Variables
 UBYTE bkg_x;
 UBYTE bkg_y;
+
+// estructura basica de un personaje, id, posicion, grafico
+typedef struct {
+	UBYTE id;
+	UBYTE x;
+	UBYTE y;
+	UBYTE graphic;
+} Character;
+
+Character PG;
+UBYTE sprPlayer; // variable para guardar la memoria del sprite
 
 //Functions
 void game();
 void init();
 void init_game_var();
+void init_game_sprite();
 void init_game_bkg();
-void init_game_gui();
+//void init_game_gui();
 
 
 void main() {
@@ -45,6 +61,7 @@ void init() {
     init_game_var();
     init_game_bkg();
     //init_game_gui();
+    init_game_sprite();
     DISPLAY_ON;
     enable_interrupts();
 
@@ -55,7 +72,44 @@ void init_game_var() {
     bkg_y = 0;
 }
 
+void init_game_sprite() {
+    HIDE_SPRITES;
+    SWITCH_ROM_MBC1(pg_idle_tilesetBank);
+    set_sprite_data(0, 32, pg_idle_tileset);
+    SPRITES_8x16;
+    
+    // asigna a cada sprite un tile, sprite (0-39), posicion del tile
+    set_sprite_tile( 0, 0);
+    set_sprite_tile( 0, 1);
+    set_sprite_tile( 1, 2);
+    set_sprite_tile( 1, 3);
+
+    set_sprite_tile( 4, 4);
+    set_sprite_tile( 4, 5);
+    set_sprite_tile( 5, 6);
+    set_sprite_tile( 5, 7);
+
+    set_sprite_tile( 2, 8);
+    set_sprite_tile( 2, 9);
+    set_sprite_tile( 3, 10);
+    set_sprite_tile( 3, 11);
+
+    set_sprite_tile( 6, 12);
+    set_sprite_tile( 6, 13);
+    set_sprite_tile( 7, 14);
+    set_sprite_tile( 8, 15);
+
+    //sprPlayer = loadSpriteGraphics( pg_idle_tileset, 128);
+    //PG.id = setupSprite(last_sprite_id, 4, 4 );
+    PG.id=last_sprite_id;
+    PG.x = 16;
+    PG.y = 88;
+    moveSprite( PG.id, PG.x, PG.y, 4, 4);
+    SHOW_SPRITES;
+}
+
 void init_game_bkg() {
+    HIDE_BKG;
     SWITCH_ROM_MBC1(bkg_tilesetBank);                               //Select Bank Memory
     set_bkg_data(0, 16, bkg_tileset);                               //Load 16 Tiles in VRAM
     SWITCH_ROM_MBC1(bkg_mapBank);                                   //Select Banck Memory
--- make.bat
+++ make.bat
@@ -1,11 +1,13 @@
 REM graphics
 lcc -Wf-bo1 -c dev/data/tiles/bkg_tileset.c -o bkg_tileset.o
+lcc -Wf-bo1 -c dev/data/tiles/pg_idle_tileset.c -o pg_idle_tileset.o
 REM maps
 lcc -Wf-bo2 -c dev/data/maps/bkg_map.c -o bkg_map.o
 REM logic
+lcc -c dev/utils/char_utils.c -o char_utils.o
 lcc -c dev/main.c -o main.o
 REM build
-lcc -Wl-yt1 -Wl-yo4 -Wl-ya0 -o rom/NuclearHead.gb main.o bkg_tileset.o bkg_map.o
+lcc -Wl-yt1 -Wl-yo4 -Wl-ya0 -o rom/NuclearHead.gb main.o char_utils.o pg_idle_tileset.o bkg_tileset.o bkg_map.o
 REM clean
 del *.o *.lst
 pause
