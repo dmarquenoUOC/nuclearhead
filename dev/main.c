b62ea22b9492377fde0198a2e1385f17210c5db0
dmarquenoUOC <116842281+dmarquenoUOC@users.noreply.github.com>
2022-11-05 15:57:35 +0100

    Preparar pantalla titulo

--- dev/main.c
+++ dev/main.c
@@ -2,21 +2,22 @@
 // TFG UOC Videojocs
 #include <gb/gb.h>
 #include <gb/hardware.h>
+#include "utils/char_utils.h" // rutinas de manejo de metasprites (sprite formado por varios sprites)
 
 // Tiles
 #include "data/tiles/bkg_tileset.h"
 #include "data/tiles/pg_idle_tileset.h"
+#include "data/tiles/abc_tileset.h"
 //#include "data/tiles/gui_tileset.h"
 // Maps
 #include "data/maps/bkg_map.h"
+#include "data/maps/title_map.h"
 //#include "data/maps/gui_map.h"
 
-// rutinas de manejo de metasprites (sprite formado por varios sprites)
-#include "utils/char_utils.h"
-
-//Variables
-UBYTE bkg_x;
-UBYTE bkg_y;
+//Constants
+#define TITLE_ST            0U
+#define STORY_ST            1U
+#define GAME_ST             2U
 
 // estructura basica de un personaje, id, posicion, grafico
 typedef struct {
@@ -26,85 +27,115 @@ typedef struct {
 	UBYTE graphic;
 } Character;
 
+//Variables
+UBYTE bkg_x;
+UBYTE bkg_y;
+UBYTE cur_joypad;
+UBYTE pre_joypad;
 Character PG;
-UBYTE sprPlayer; // variable para guardar la memoria del sprite
+UBYTE game_state;
+
 
 //Functions
+void title();
+void story();
 void game();
-void init();
-void init_game_var();
-void init_game_sprite();
+void init_game();
+void init_title();
+void init_story();
 void init_game_bkg();
+void init_title_bkg();
+void init_game_var();
+void init_game_sprites();
+void init_pg_sprite();
+void read_joypad();
 //void init_game_gui();
 
 
 void main() {
-    game();
+
+    //while (1) {
+        //title();
+        //story();
+        game();
+    //}
+}
+
+void title(){
+    init_title();
+    while (game_state == TITLE_ST) {
+        wait_vbl_done();
+    }
+
+}
+
+void init_title(){
+    
+    disable_interrupts();
+    DISPLAY_OFF;
+    game_state = TITLE_ST;
+    init_title_bkg();
+    DISPLAY_ON;
+    enable_interrupts();
+    wait_vbl_done();
+
+}
+
+void init_title_bkg() {
+    HIDE_BKG;
+    SWITCH_ROM_MBC1(abc_tilesetBank);
+    set_bkg_data(0, 48, abc_tileset);
+    SWITCH_ROM_MBC1(title_mapBank);
+    set_bkg_tiles(0, 0, title_mapWidth, title_mapHeight, title_mapBank);
+    move_bkg(0, 0);
+    SHOW_BKG;
+}
+
+void story(){
+
+    init_story();
+    while (game_state == STORY_ST) {
+        wait_vbl_done();
+    }
+
+}
+
+void init_story(){
+
 }
 
 void game() {
     
-    init();
-
-    while (1) {
-        
-        // sincroniza la pantalla
-	    wait_vbl_done();
-        
+    init_game();
+    while (game_state == GAME_ST) {
+        wait_vbl_done();
     }
+        
 }
 
-void init() {
+void init_game() {
 	
 	disable_interrupts();
     DISPLAY_OFF;
     init_game_var();
     init_game_bkg();
     //init_game_gui();
-    init_game_sprite();
+    init_game_sprites();
     DISPLAY_ON;
     enable_interrupts();
+    wait_vbl_done();
 
 }
 
 void init_game_var() {
     bkg_x = 0;
     bkg_y = 0;
+    game_state=GAME_ST;
 }
 
-void init_game_sprite() {
+void init_game_sprites() {
     HIDE_SPRITES;
-    SWITCH_ROM_MBC1(pg_idle_tilesetBank);
-    set_sprite_data(0, 32, pg_idle_tileset);
-    SPRITES_8x16;
-    
-    // asigna a cada sprite un tile, sprite (0-39), posicion del tile
-    set_sprite_tile( 0, 0);
-    set_sprite_tile( 0, 1);
-    set_sprite_tile( 1, 2);
-    set_sprite_tile( 1, 3);
-
-    set_sprite_tile( 4, 4);
-    set_sprite_tile( 4, 5);
-    set_sprite_tile( 5, 6);
-    set_sprite_tile( 5, 7);
-
-    set_sprite_tile( 2, 8);
-    set_sprite_tile( 2, 9);
-    set_sprite_tile( 3, 10);
-    set_sprite_tile( 3, 11);
-
-    set_sprite_tile( 6, 12);
-    set_sprite_tile( 6, 13);
-    set_sprite_tile( 7, 14);
-    set_sprite_tile( 8, 15);
-
-    //sprPlayer = loadSpriteGraphics( pg_idle_tileset, 128);
-    //PG.id = setupSprite(last_sprite_id, 4, 4 );
-    PG.id=last_sprite_id;
-    PG.x = 16;
-    PG.y = 88;
-    moveSprite( PG.id, PG.x, PG.y, 4, 4);
+    init_pg_sprite();
     SHOW_SPRITES;
 }
 
@@ -116,6 +147,40 @@ void init_game_bkg() {
     set_bkg_tiles(0, 0, bkg_mapWidth, bkg_mapHeight, bkg_map);      //Set tiles into Background Layer
     move_bkg(bkg_x, bkg_y);
     SHOW_BKG;
+}
+
+void init_pg_sprite(){
+
+    SWITCH_ROM_MBC1(pg_idle_tilesetBank);
+    set_sprite_data(0, 32, pg_idle_tileset);
+    SPRITES_8x16;
+    // asigna a cada sprite un tile, sprite (0-31), posicion del tile
+    set_sprite_tile( 0, 0);
+    set_sprite_tile( 0, 1);
+    set_sprite_tile( 1, 2);
+    set_sprite_tile( 1, 3);
+    set_sprite_tile( 4, 4);
+    set_sprite_tile( 4, 5);
+    set_sprite_tile( 5, 6);
+    set_sprite_tile( 5, 7);
+    set_sprite_tile( 2, 8);
+    set_sprite_tile( 2, 9);
+    set_sprite_tile( 3, 10);
+    set_sprite_tile( 3, 11);
+    set_sprite_tile( 6, 12);
+    set_sprite_tile( 6, 13);
+    set_sprite_tile( 7, 14);
+    set_sprite_tile( 7, 15);
+    PG.id=last_sprite_id;
+    PG.x = 16;
+    PG.y = 96;
+    moveSprite( PG.id, PG.x, PG.y, 4, 4);
+
+}
+
+void read_joypad() {
+    pre_joypad = cur_joypad;
+    cur_joypad = joypad();
 }
 
 /*
